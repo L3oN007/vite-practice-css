d208aac4295c9f219dcea73be57eea140ec97e54
Minh Nhật <89580650+L3oN007@users.noreply.github.com>
2024-01-22 00:03:47 +0700

    Apply header to home page

--- src/pages/Home/_components/header.tsx
+++ src/pages/Home/_components/header.tsx
@@ -2,51 +2,50 @@ import { Button } from "@/components/ui/button";
 import {
   NavigationMenu,
   NavigationMenuContent,
-  NavigationMenuIndicator,
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
   NavigationMenuTrigger,
-  NavigationMenuViewport,
+  navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
-import { cn } from "@/lib/utils";
-import { twMerge } from "tailwind-merge";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <div className="border-b-1 fixed top-0 z-10 flex h-12 w-full items-center justify-between border px-7 shadow-sm">
+    <div className="border-b-1 fixed top-0 z-10 flex h-16 w-full items-center justify-between border px-7 shadow-sm">
       <div className="flex items-center justify-center ">
         <div className="">
-          <img src="./logo.png" alt="Logo" className="h-5 w-5 cursor-pointer" />
+          <img src="./logo.png" alt="Logo" className="h-7 w-7 cursor-pointer" />
         </div>
-        <NavigationMenu>
+        <NavigationMenu className="ml-4">
           <NavigationMenuList>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Product</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>Link</NavigationMenuLink>
               </NavigationMenuContent>
             </NavigationMenuItem>
             <NavigationMenuItem>
-              <NavigationMenuTrigger>Item two</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <NavigationMenuLink>Link2</NavigationMenuLink>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+              <NavigationMenuTrigger>Download</NavigationMenuTrigger>
               <NavigationMenuContent>
                 <NavigationMenuLink>Link</NavigationMenuLink>
               </NavigationMenuContent>
+            </NavigationMenuItem>
+            <NavigationMenuItem>
+              <Link to="/document">
+                <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                  Document
+                </NavigationMenuLink>
+              </Link>
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
       </div>
-      <div className="flex space-x-2">
-        <Button size={"sm"} variant="ghost">
-          Register
+      <div className="flex space-x-2 font-semibold">
+        <Button size={"default"} variant="ghost">
+          Login
         </Button>
-        <Button size={"sm"}>Login</Button>
+        <Button size={"default"}>Get free account</Button>
       </div>
     </div>
   );
